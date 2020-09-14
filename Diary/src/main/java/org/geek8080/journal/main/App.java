5eeb4d160f3d2acffd6bdcb9c35c3c23d95c6a64
Prashant Prakash <prashantprakash97@gmail.com>
2020-09-14 15:28:26 +0530

    Finalized OTP Verification Window

--- Diary/src/main/java/org/geek8080/journal/main/App.java
+++ Diary/src/main/java/org/geek8080/journal/main/App.java
@@ -1,24 +1,20 @@
 package org.geek8080.journal.main;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.application.Preloader;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 import org.geek8080.journal.entities.Diary;
 import org.geek8080.journal.entities.Page;
 import org.geek8080.journal.entities.User;
-import org.geek8080.journal.main.account.Launcher;
+import org.geek8080.journal.main.account.LogInController;
 import org.geek8080.journal.main.account.OTPController;
 import org.geek8080.journal.services.Authenticator;
 import org.geek8080.journal.services.Database;
-import org.geek8080.journal.utils.FileHandler;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,28 +25,32 @@ public class App extends Application {
 	public static Authenticator AUTH;
 	public static Diary DIARY;
 	public static boolean firstUse = false;
-	public static boolean verifiedOTP = false;
 
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		if (firstUse){
-			Parent root = FXMLLoader.load(getClass().getResource("account/OTP.fxml"));
-			Scene scene = new Scene(root);
-			stage.setScene(scene);
-			stage.initStyle(StageStyle.UNDECORATED);
+		Parent root;
+
+		if (firstUse) {
+			// launch OTP window
+			root = FXMLLoader.load(getClass().getResource("account/OTP.fxml"));
 			stage.setTitle("OTP Verification");
-			stage.setOnCloseRequest((e) -> {
-				System.exit(0);
-			});
 			OTPController.pstage = stage;
-			stage.show();
 		}else {
-			Parent root = FXMLLoader.load(getClass().getResource("account/LogIn.fxml"));
-			Scene scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
+			// Launch log in window
+			root = FXMLLoader.load(getClass().getResource("account/LogIn.fxml"));
+			stage.setTitle("Log In");
+			LogInController.pstage = stage;
 		}
+
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.initStyle(StageStyle.UNDECORATED);
+		stage.show();
+		stage.setOnCloseRequest(e -> {
+			System.exit(0);
+		});
+
 	}
 
 	@Override
