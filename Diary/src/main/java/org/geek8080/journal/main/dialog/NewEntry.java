e3c50144feeed3cb1a012f5e629cb086cdacb44b
Prashant Prakash <prashantprakash97@gmail.com>
2020-09-15 13:18:15 +0530

    Finalized code to Generate new Pages in the Diary

--- Diary/src/main/java/org/geek8080/journal/main/dialog/NewEntry.java
+++ Diary/src/main/java/org/geek8080/journal/main/dialog/NewEntry.java
@@ -31,64 +31,22 @@ public class NewEntry extends StackPane {
 	}
 
 	@FXML
-	private StackPane rootStack;
+	public StackPane rootStack;
 
 	@FXML
-	private JFXTextField titleField;
+	public JFXTextField titleField;
 
 	@FXML
-	private JFXTextField subtitleField;
+	public JFXTextField subtitleField;
 
 	@FXML
-	private JFXTextArea bodyField;
+	public JFXTextArea bodyField;
 
 	@FXML
 	public JFXButton cancelButton;
 
 	@FXML
-	private JFXButton saveButton;
-
-	@FXML
-	private void save(MouseEvent event){
-		String title = titleField.getText().trim();
-		String subTitle = subtitleField.getText().trim();
-		String body = bodyField.getText().trim();
-		if( (title == null || title.equalsIgnoreCase("")) || (body == null || body.equalsIgnoreCase(""))){
-			Pane p = new Pane();
-			p.setPrefWidth(500);
-			p.prefHeight(200);
-			Text text = new Text("Titile and Body are compulsory, don't leave them blank. If they are not blank then report the bug.");
-			text.setWrappingWidth(500);
-			JFXDialog errorDialog = new JFXDialog(rootStack, p, JFXDialog.DialogTransition.CENTER);
-			errorDialog.show();
-			errorDialog.setOnDialogClosed(e -> {
-				return;
-			});
-		}
-		String query = "";
-		HashMap<Integer, Object> values = new HashMap<>();
-		values.put(1, title);
-		if(subTitle == null || subTitle.equalsIgnoreCase("")){
-			query = "INSERT INTO PAGE(TITLE, BODY) VALUES(?, ?)";
-			values.put(2, body);
-		}else{
-			query = "INSERT INTO PAGE(TITLE, SUBTITLE, BODY) VALUES(?, ?, ?)";;
-			values.put(2, subTitle);
-			values.put(3, body);
-		}
-		try{
-			App.DB.executeQuery(query, values);
-			Alert alert = new Alert(Alert.AlertType.INFORMATION);
-			alert.setTitle("Successfull Operation");
-			alert.setContentText("Entered value into DB successfully");
-			alert.showAndWait();
-		}catch (RuntimeException ex){
-			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setTitle("Unsuccessfull Operation");
-			alert.setContentText("Couldn't enter value into DB successfully, refer logs..." + ex.getMessage());
-			alert.showAndWait();
-		}
-	}
+	public JFXButton saveButton;
 
 }
 
